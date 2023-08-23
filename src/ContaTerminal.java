4a08eca2299f993788ff2dbce1e86ac651b270fa
Levy Pontes <86980955+levygust@users.noreply.github.com>
2023-08-23 11:58:42 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,19 +4,19 @@
             public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
                                
-                System.out.println("Por favor, digite o nome do cliente!!");
+                System.out.println("Por favor, digite o nome do cliente!");
                 String nomeCliente = sc.nextLine(); 
                   
         
-                System.out.println("Por favor, digite a agência!!");
+                System.out.println("Por favor, digite a agência!");
                 String agencia = sc.nextLine();
         
-                System.out.println("Por favor, digite o número da conta!!");
+                System.out.println("Por favor, digite o número da conta!");
                 int numero = sc.nextInt();
                 //clearBuffer(sc);
                 
                
-                System.out.println("Por favor, digite o saldo!!");
+                System.out.println("Por favor, digite o saldo!");
                 Double saldo = sc.nextDouble();
                 
         
